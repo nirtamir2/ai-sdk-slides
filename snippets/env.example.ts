6c13a200bd1184ea2bee46be607e93997ae8db2c
Nir Tamir <nirtamir2@gmail.com>
2024-06-17 01:07:09 +0300

    wip - prettier and run gemini in the slide

---
+++ snippets/env.example.ts
@@ -0,0 +1,4 @@
+export const env:Record<"GOOGLE_GENERATIVE_AI_API_KEY", string>  = {
+    GOOGLE_GENERATIVE_AI_API_KEY: "YOUR_KEY",
+  };
+  
--- snippets/external.ts
+++ snippets/external.ts
@@ -1,17 +1,5 @@
 /* eslint-disable no-console */
 
-// #region snippet
-// Inside ./snippets/external.ts
-import { google } from "@ai-sdk/google";
-import { generateText } from "ai";
-
- const result = await generateText({
-    model: google("models/gemini-1.5-flash-latest"),
-    prompt: "Tell me a joke.",
-  });
-
-  console.log(result.text);
-// #endregion snippet
 
 export function sayHello() {
   console.log('Hello from snippets/external.ts')
