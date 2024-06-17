84cc5bc84506a542b2ca8c47deb96e6f4995dda7
Nir Tamir <nirtamir2@gmail.com>
2024-06-17 04:42:47 +0300

    wip ai

---
+++ setup/shortcuts.ts
@@ -0,0 +1,13 @@
+import type { NavOperations, ShortcutOptions } from '@slidev/types'
+import { defineShortcutsSetup } from '@slidev/types'
+
+export default defineShortcutsSetup((nav: NavOperations, base: ShortcutOptions[]) => {
+  return [
+    ...base,
+    {
+      key: 'e',
+      fn: () => nav.goLast(),
+      autoRepeat: true,
+    }
+  ]
+})
